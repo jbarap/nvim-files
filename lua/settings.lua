80c4261c604778cd75424ba7c4199536220407fc
Juan Barajas <jbperches@gmail.com>
2021-09-01 21:25:37 -0500

    settings are now set with vim.opt

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,79 +1,80 @@
 local cmd = vim.cmd
 local utils = require("utils")
-local set_opt = require("utils").set_opt
+local opt = vim.opt
 
 -- Split direction
-set_opt("o", "splitbelow", true)
-set_opt("o", "splitright", true)
+opt.splitbelow = true
+opt.splitright = true
 
 -- Resize sign column limit
-set_opt("w", "signcolumn", "yes:1")
+opt.signcolumn = "yes:1"
 
 -- Color line/column
-set_opt("w", "colorcolumn", "90")
-set_opt("w", "cursorline", true)
+opt.colorcolumn = "90"
+opt.cursorline = true
 
 -- Completion menu
-set_opt("o", "pumheight", 20)
-set_opt("o", "pumblend", 10)
+opt.pumheight = 20
+opt.pumblend = 10
 
 -- Set encoding
-set_opt("o", "encoding", "utf-8")
+opt.encoding = "utf-8"
 
 -- Hidden buffers to switch buffers without saving
-set_opt("o", "hidden", true)
+opt.hidden = true
 
 -- Enable mouse support
-set_opt("o", "mouse", "a")
+opt.mouse = "a"
 
 -- Auto read file changes
-set_opt("o", "autoread", true)
+opt.autoread = true
 
 -- Make last window always have a status line
-set_opt("o", "laststatus", 2)
+opt.laststatus = 2
 
 -- Indent
-set_opt("b", "softtabstop", 4)
-set_opt("b", "shiftwidth", 4)
-set_opt("b", "expandtab", true)
-set_opt("b", "autoindent", true)
-set_opt("b", "smartindent", true)
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 
 -- Wrap behavior
-set_opt("w", "wrap", false)
+opt.wrap = false
 
 -- Line numbers
-set_opt("w", "number", true)
+opt.number = true
 
 -- Folding (with Treesitter)
--- set_opt("w", "foldmethod", "expr")
--- set_opt("w", "foldexpr", "nvim_treesitter#foldexpr()")
--- set_opt("w", "foldlevel", 99)
+-- opt.foldmethod = "expr"
+-- opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- opt.foldlevel = 99
 
 -- Search
-set_opt("o", "hlsearch", true)
-set_opt("o", "incsearch", true)
-set_opt("o", "ignorecase", true)
-set_opt("o", "smartcase", true)
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Faster update time
-set_opt("o", "updatetime", 200)
+opt.updatetime = 200
 
 -- Scroll offsets
-set_opt("o", "scrolloff", 7)
-set_opt("o", "sidescrolloff", 4)
+opt.scrolloff = 7
+opt.sidescrolloff = 4
 
 -- Fillchars
-set_opt("o", "fillchars", "diff:╱")
-set_opt("w", "list", true)
-set_opt("w", "listchars", [[tab:  ]])
+opt.fillchars = "diff:╱"
+opt.list = true
+opt.listchars = {tab = "  "}
 
 -- Enable filetype plugin
 cmd("filetype plugin on")
 
 -- Diff options
 -- waiting on: https://github.com/neovim/neovim/pull/14537
-set_opt("o", "diffopt", "filler,vertical,closeoff,internal,indent-heuristic,algorithm:patience")
+-- oddly enough, this option isn't set as a table
+opt.diffopt = "filler,vertical,closeoff,internal,indent-heuristic,algorithm:patience"
 
 -- Folds
 function _G.custom_fold_expr()
@@ -81,7 +82,7 @@ function _G.custom_fold_expr()
   local sub = vim.fn.substitute(line, [[/*|*/|{{{\d=]], '', 'g')
   return sub .. ' (' .. tostring(vim.v.foldend - vim.v.foldstart) .. ' lines)'
 end
-set_opt('o', 'foldtext', 'v:lua.custom_fold_expr()')
+opt.foldtext = "v:lua.custom_fold_expr()"
 
 -- Highlight text on yank
 utils.create_augroup({
