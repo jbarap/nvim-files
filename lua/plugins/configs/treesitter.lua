eb4ef0d5387267cd1a6d692c1c8d0a3f93602293
Juan Barajas <jbperches@gmail.com>
2022-12-05 19:37:16 -0600

    disable treesitter on big files to improve performance

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,44 +1,66 @@
+local disabled_buffers = {}
+
+local function disable_on_big_file(lang, bufnr)
+  local stat = vim.loop.fs_stat(vim.api.nvim_buf_get_name(bufnr))
+  if stat == nil then
+    return false
+  end
+
+  local is_big = stat.size > 5000000 -- disable on files > 5MB
+
+  if is_big and disabled_buffers[bufnr] == nil then
+    vim.notify("File is greater than 5MB, disabling treesitter for improved performance.", vim.log.levels.INFO)
+    disabled_buffers[bufnr] = true
+  end
+
+  return is_big
+end
+
 require("nvim-treesitter.configs").setup({
   ensure_installed = "all",
 
   -- highlight performance is slow on big files
   highlight = {
     enable = true,
-    disable = {},
+    disable = disable_on_big_file,
   },
 
   playground = {
     enable = true,
+    disable = disable_on_big_file,
   },
 
   incremental_selection = {
     enable = true,
+    disable = disable_on_big_file,
     keymaps = {
-      init_selection = '<CR>',
-      scope_incremental = '<CR>',
-      node_incremental = '<TAB>',
-      node_decremental = '<S-TAB>',
+      init_selection = "<CR>",
+      scope_incremental = "<CR>",
+      node_incremental = "<TAB>",
+      node_decremental = "<S-TAB>",
     },
   },
 
   -- indenting with TS is really slow for some reason, look for alternatives
   indent = {
     enable = false,
-    disable = { "python" },
+    disable = disable_on_big_file,
   },
 
   yati = {
     enable = true,
+    disable = disable_on_big_file,
     default_lazy = true,
     default_fallback = function(lnum, computed, bufnr)
       -- TODO: check what tmindent returns when it fails, and add default as second fallback
-      return require('tmindent').get_indent(lnum, bufnr) + computed
+      return require("tmindent").get_indent(lnum, bufnr) + computed
     end,
   },
 
   textobjects = {
     select = {
       enable = true,
+      disable = disable_on_big_file,
       lookahead = true,
       keymaps = {
         ["af"] = "@function.outer",
@@ -59,6 +81,7 @@ require("nvim-treesitter.configs").setup({
     },
     move = {
       enable = true,
+      disable = disable_on_big_file,
       set_jumps = false, -- whether to set jumps in the jumplist
       goto_next_start = {
         ["]f"] = "@function.outer",
@@ -84,10 +107,11 @@ require("nvim-treesitter.configs").setup({
 
     lsp_interop = {
       enable = true,
+      disable = disable_on_big_file,
       border = "rounded",
       peek_definition_code = {
         ["<leader>pd"] = "@function.outer",
       },
-    }
+    },
   },
 })
